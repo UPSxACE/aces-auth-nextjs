bd8afd02e9446c37cb8e99e38a8ba769eed1258e
Eduardo Botelho <eduardo11224b@gmail.com>
2025-07-17 18:47:35 +0100

    initial setup with nextjs, chakra ui and biome

---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+import { Stack, Text } from "@chakra-ui/react";
+
+export default function Home() {
+  return (
+			<div>
+				<Stack>
+					<Text textStyle="xs">Hello World</Text>
+					<Text textStyle="sm">Hello World</Text>
+					<Text textStyle="md">Hello World</Text>
+					<Text textStyle="lg">Hello World</Text>
+					<Text textStyle="xl">Hello World</Text>
+					<Text textStyle="2xl">Hello World</Text>
+					<Text textStyle="3xl">Hello World</Text>
+					<Text textStyle="4xl">Hello World</Text>
+					<Text textStyle="5xl">Hello World</Text>
+					<Text textStyle="6xl">Hello World</Text>
+					<Text textStyle="7xl">Hello World</Text>
+				</Stack>
+			</div>
+		);
+}
